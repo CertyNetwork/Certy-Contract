ef385f500d213ca76783ebf6ae1ca9b5bf198a18
Edric Ngo <edricngovn@gmail.com>
2022-06-22 03:03:29 +0700

    Add readme, sample job detail

--- certy-cecareer/src/job.rs
+++ certy-cecareer/src/job.rs
@@ -4,10 +4,15 @@ pub type JobId = String; //We use 128-bit integers for the job ID, can be change
 #[derive(BorshDeserialize, Clone, BorshSerialize, Serialize, Deserialize, Debug)]
 #[serde(crate = "near_sdk::serde")]
 pub struct JobMetadata {
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub issued_at: Option<u64>, // When Job was created, Unix epoch in milliseconds
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub updated_at: Option<u64>, // When Job was last updated, Unix epoch in milliseconds
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub extra: Option<String>, // anything extra the Job wants to store on-chain. Can be stringified JSON.
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub reference: Option<String>, // URL to an off-chain JSON file with more info.
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub reference_hash: Option<Base64VecU8>, // Base64-encoded sha256 hash of JSON from reference field. Required if `reference` is included.
 }
 
